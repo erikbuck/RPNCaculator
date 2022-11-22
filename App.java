c17e710007b5f8f83076eef6dffb10c602ca67e8
erikbuck <erik.buck@wright.edu>
2022-11-21 19:03:57 -0500

    Added formatted javadoc comments as demosntration of version change

--- App.java
+++ App.java
@@ -1,13 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+/** 
  * This class exists to test the Calculator class.
+*/
 public class App {
     private static HashMap<String, Calculator.Command> commands = new HashMap<String, Calculator.Command>();
 
-    // Open a file at a specific filesystem location as an InputStream
-    // and pass the InputStream readLineByLineExample2. Then output the
-    // number of lines read
+    /**
+     * Open a file at a specific filesystem location as an InputStream
+     * and pass the InputStream readLineByLineExample2. Then output the
+     * number of lines read
+     * 
+     * @param args Command line arguments searated by white space
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         commands.put("+", new AddCommand());
         commands.put("*", new MultiplyCommand());
